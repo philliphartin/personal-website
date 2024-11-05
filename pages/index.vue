65fe4a017de35e62c422493affea0c371c31a7ba
Phillip Hartin <phillip@littlefox.studio>
2024-11-05 22:33:09 +1100

    Replace existing pages with simplified site

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,48 @@
 <template>
-  <section class="blend-overlay flex min-h-screen w-full flex-col content-center justify-center text-center font-serif text-white">
-    <div class="container mx-auto mb-12 mt-24 pl-8 pr-8 md:w-1/2">
-      <p class="mb-4 md:text-xl">Thanks for visiting my little slice of the internet.</p>
-      <p class="mb-4 md:text-xl"></p>
-      <p class="mb-4 md:text-xl">
-        I'm the Technical Director and Co-Founder of
-        <a href="https://www.littlefox.studio" class="text-gray-400">Littlefox Studio</a>
-        , a venture studio, where we obsess over compelling digital products.
-      </p>
-      <p class="mb-6 md:text-xl">
-        My work keeps me quite busy, but I'm always keen to hear about another interesting project, so don't hesitate to reach out over email or socials.
-      </p>
+  <div class="bg-dotted flex min-h-screen flex-col items-center justify-center">
+    <!-- <div class="flex"> -->
+    <NuxtImg
+      src="/images/headshot.jpg"
+      sizes="100vw sm:50vw md:400px"
+      class="mb-6 size-32 rounded-full sm:size-48" />
 
-      <a class="mx-2 text-lg" href="mailto:phillip@littlefox.studio">Email</a>
-
-      <a class="mx-2 text-lg" href="https://www.linkedin.com/in/philliphartin/">LinkedIn</a>
-
-      <a class="mx-2 text-lg" href="https://twitter.com/philliphartin">X</a>
+    <div class="container text-center ">
+      <h2 class="text-4xl font-bold sm:text-6xl ">
+        Phillip Hartin<span class="ml-2 text-base text-gray-500">PhD</span>
+      </h2>
+      <h1 class="mt-2 font-light sm:text-lg">
+        Building digital products, services, and companies.
+      </h1>
     </div>
-  </section>
+
+    <footer
+      class="fixed bottom-0 my-12 flex w-fit gap-8 rounded-full border border-black/[8%] bg-white px-6 py-3.5 font-mono text-sm font-light text-gray-500 shadow-sm">
+      <NuxtLink
+        v-for="link in links"
+        :key="link.label"
+        :to="link.to"
+        class="hover:underline">{{ link.label }}</NuxtLink>
+    </footer>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const links = ref([
+  {
+    label: 'GitHub',
+    to: 'https://github.com/philliphartin',
+  },
+  {
+    label: 'LinkedIn',
+    to: 'https://www.linkedin.com/in/philliphartin/',
+  },
+  {
+    label: 'Airlume',
+    to: 'https://airlume.cloud',
+  },
+  {
+    label: 'Laravel',
+    to: 'https://laravel.com',
+  },
+])
+</script>
