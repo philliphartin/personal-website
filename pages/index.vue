32b5d56b331318e40c3e41b50affa2a7520d0c8a
Phillip Hartin <phillip@littlefox.studio>
2024-11-14 21:31:59 +1100

    Simplify

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,24 @@
 <template>
   <div class="bg-dotted flex min-h-screen flex-col items-center justify-center">
-    <!-- <div class="flex"> -->
     <NuxtImg
       src="/images/phillip.jpg"
       sizes="100vw sm:50vw md:400px"
       class="mb-6 size-32 rounded-full sm:size-48" />
 
     <div class="container text-center ">
-      <h2 class="text-4xl font-bold sm:text-6xl ">
+      <h2 class="text-5xl font-bold">
         Phillip Hartin<span class="ml-2 text-base text-gray-500">PhD</span>
       </h2>
-      <h1 class="mt-2 font-light sm:text-lg">
-        Building digital products, services, and companies.
-      </h1>
     </div>
 
-    <footer
-      class="fixed bottom-0 my-12 flex w-fit gap-8 rounded-full border border-black/[8%] bg-white px-6 py-3.5 font-mono text-sm font-light text-gray-500 shadow-sm">
+    <div
+      class="my-4 flex w-fit gap-8 font-mono text-sm font-light text-gray-500">
       <NuxtLink
         v-for="link in links"
         :key="link.label"
         :to="link.to"
-        class="hover:underline">{{ link.label }}</NuxtLink>
-    </footer>
+        class="hover:text-gray-900 hover:underline">{{ link.label }}</NuxtLink>
+    </div>
   </div>
 </template>
 
